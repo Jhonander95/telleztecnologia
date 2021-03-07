69a7dfa4c3963af4d165fb6286aa892d0046655e
Jhonanderson Tellez <tellezjhonander@gmail.com>
2021-03-07 15:42:59 -0500

    creqando un input y probando la interpolacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
