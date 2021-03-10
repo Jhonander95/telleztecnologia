5e705e6e48ab4c437b48203f8e184c3c2ccfc68b
Jhonanderson Tellez <tellezjhonander@gmail.com>
2021-03-09 21:54:34 -0500

    se agregaron funciones al carrito

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,12 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { style } from '@angular/animations';
+import { 
+	Component, 
+	Input, 
+	Output, 
+	EventEmitter, 
+	SimpleChanges, 
+	OnInit, 
+	DoCheck, 
+	OnDestroy 
+} from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
-    templateUrl: './product.component.html'
+    templateUrl: './product.component.html',
+		styleUrls: ['./product.component.scss']
 })
 
-export  class ProductComponent { 
+export  class ProductComponent implements DoCheck, OnInit, OnDestroy { 
 
-    @Input() product: Product = new Product();
+   @Input() product: Product;
+   @Output()  productClicked: EventEmitter<any> = new EventEmitter();
+
+   constructor() {
+    console.log('1. constructor');
+   }
+
+ /*   ngOnChanges(changes: SimpleChanges){
+       console.log('2. ngOnChanges');
+       console.log(changes);     
+   } */
+
+   ngOnInit(){
+    console.log('3. ngOnInit');
+   }
+
+   ngDoCheck(){
+       console.log('4. ngDoCheck');  
+   }
+
+	ngOnDestroy(){
+		console.log('ngOnDestroy');
+		
+	}
+   addCart() {
+       console.log('Agregado al carrito ');
+       this.productClicked.emit(this.product.id);
+   }
 }
